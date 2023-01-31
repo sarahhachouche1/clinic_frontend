8c46123c630483217fdb42e39872eba74c6749ce
Youssef <youssefhamadi94@gmail.com>
2023-01-31 16:52:40 +0200

    first commit about page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header.jsx";
 import { Home } from "./components/Home/Home";
-import { About } from "./components/About/About";
+import  About  from "./components/About/About";
 import { Services } from "./components/Services/Services";
 import { Reviews } from "./components/Reviews/Reviews";
 import { Contact } from "./components/Contact/Contact";
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
     <BrowserRouter>
-      <Header />
+      <Header/>
       <main className="main">
       <Routes>
         <Route path="/" element={<Home />}></Route>
@@ -22,7 +22,7 @@ function App() {
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
       </main>
-      <Footer />
+      <Footer/>
     </BrowserRouter>
     </div>
   );
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,27 @@
-import React from "react";
-import "../../styles/About.css";
+import React from 'react'
+import "../../styles/About.css"
+import Vector204 from "../../images/Vector204.png"
+import Group_1 from "../../images/Group_1.png"
 
-export const About = () => {
-  return <div className="about-page">About</div>;
-};
+
+const About = () => {
+  return (
+    <>
+    <div>
+      <h1 className='H1Y'>Passion for <span className='DentistryY'>Dentistry</span> <br/>since 2015 </h1>
+      <p className='PY'>DENTI Clinic was established in 1977, when Professor Hani Khoury returned to Lebanon from France, after finishing his specialty in gum and bone disease, also known as “Periodontics”.</p>
+    
+    </div>
+
+    <div> 
+      <img className='imgY'src={Vector204} alt="Vector204"/>
+    </div>
+    <div>
+      <img className='imgRy' src={Group_1} alt="imgRy"/>
+      <h1 className='pBY'>MEET OUR DOCTORS</h1>
+    </div>
+  </>
+  )
+}
+
+export default About
