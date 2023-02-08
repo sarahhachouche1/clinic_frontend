8ddb739574c3363a728eec29146496c631d8135e
Youssef <youssefhamadi94@gmail.com>
2023-02-08 14:02:06 +0200

    fixed about-review page final responsivness

---
+++ src/components/Buttons/Buttonappt.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/Button.css";
+import "../../styles/About.css";
+
+export const Buttonappt = () => {
+  return <div className="Seemoree buttonA ">Book Appointment</div>;
+};
+
+
+
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "../../styles/Reviews.css";
 import ReviewsHeader from "../../images/ReviewsHeader.png"
-import { Button } from "../Button/Button";
+import { Button } from "../Buttons/Button";
+import { Buttonappt } from "../Buttons/Buttonappt";
 export const Reviews = () => {
   return (
     <>
@@ -10,11 +11,12 @@ export const Reviews = () => {
 
 <img className='Reviews_Header_img'src={ReviewsHeader} alt="ReviewsHeader"/>
 
+ <Button/> 
+ {/* <Buttonappt /> */}
 
-<Button/>
+</div>
 <div>
 <h2 className="Hone_Reviews">Check Out <span className="Big_Font_Reviews"><span className="Recent_Blue"> <br/> Recent </span>Reviews</span></h2>
-</div>
 </div>
 </>
 
